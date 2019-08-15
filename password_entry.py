6080b6047c362ccf101aeac2ef878569f660032b
Bailee <bailee.brown@my.jcu.edu.au>
2019-08-15 17:22:11 +1000

    Add password checker

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,11 @@
 """Bailee Brown"""
+
+MIN_LENGTH = 6
+
+password = input("Please enter a password 6 digits or longer: ")
+
+while len(password) < MIN_LENGTH:
+    print("Password not long enough,")
+    password = input("Please enter a valid password: ")
+
+print(len(password)*'*')
